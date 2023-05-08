deef91041001d6f67caf988b9d4cb8dc5b9abfb8
Noufal Khan <noufalkhank@gmail.com>
2023-05-08 16:43:57 +0530

    variable added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        "title": [
+          "3.5rem",
+          {
+            lineHeight: "3rem",
+            letterSpacing: "-0.01em",
+            fontWeight: "700",
+          },
+        ],
+        "subtitle": [
+          "1.875rem",
+          {
+            lineHeight: "2.25rem",
+            letterSpacing: "-0.02em",
+            fontWeight: "500",
+          },
+        ],
+      },
+      colors: {
+        'accent' : '#fbab92'
+      },
+    },
   },
   plugins: [],
-}
+};
